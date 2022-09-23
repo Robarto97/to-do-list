1178a953a0ab453ac18b9add3bf74a0a72fc5611
Robarto <robartpetrov@gmail.com>
2022-09-23 19:53:24 +0300

    Finished To Do List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [toDo, setToDo] = useState(null);
+
+  const [newTask, setNewTask] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/tasks")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setToDo(data);
+      });
+  }, []);
+
+  const addTask = () => {
+    if (newTask) {
+      let index = toDo.length + 1;
+      let newEntry = { id: index, title: newTask };
+      setToDo([...toDo, newEntry]);
+      setNewTask("");
+    }
+  };
+
+  const deleteTask = (id) => {
+    let newTasks = toDo.filter((task) => task.id !== id);
+    setToDo(newTasks);
+  };
+
+  const handleKey = (event) => {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To do list</h1>
+      <div className="row">
+        <input
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          onKeyDown={handleKey}
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <ul className="tasks">
+        {toDo && toDo.length ? "" : "No Tasks!"}
+        {toDo &&
+          toDo.map((task, index) => {
+            return (
+              <React.Fragment key={task.id}>
+                <li className="task">
+                  {index + 1}. {task.title}
+                  <span onClick={() => deleteTask(task.id)}>
+                    <FontAwesomeIcon icon={faTrashCan} />
+                  </span>
+                </li>
+              </React.Fragment>
+            );
+          })}
+      </ul>
     </div>
   );
 }
